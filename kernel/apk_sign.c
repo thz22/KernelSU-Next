20b202026a4a77cf7500d39d4845ea3d5dc27085
AzyrRuthless <muhammadriza717@gmail.com>
2025-01-11 16:18:05 +0700

    Kernel: Implement SUSFS v1.5.4+

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -314,7 +314,8 @@ module_param_cb(ksu_debug_manager_uid, &expected_size_ops,
 
 #endif
 
-bool is_manager_apk(char *path)
+bool ksu_is_manager_apk(char *path)
 {
-	return check_v2_signature(path, EXPECTED_NEXT_SIZE, EXPECTED_NEXT_HASH);
+	return (check_v2_signature(path, EXPECTED_NEXT_SIZE, EXPECTED_NEXT_HASH) || check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH) || check_v2_signature(path, EXPECTED_XX_SIZE, EXPECTED_XX_HASH));
+
 }
